aa619d60a6b5bbf22f863695e44411caf1c16ac8
purin52002 <purin7635@gmail.com>
2020-01-24 21:17:40 +0900

    add image

--- config.py
+++ config.py
@@ -11,8 +11,9 @@ gdrive_path = Path(r'/content/drive/My Drive')
 
 class ImagePath:
     image_dir_path = gdrive_path/'Image'
-    image_path_dict = {'salad': str(image_dir_path/'salad.jpg'),
-                       'katsudon': str(image_dir_path/'katsudon.jpg')}
+    image_path_dict = {image_name: str(image_dir_path/f'{image_name}.jpg')
+                       for image_name in ['salad', 'katsudon',
+                                          'farm', 'flower', 'watarfall']}
 
 
 class DirectoryPath:
