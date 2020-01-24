91d71b110770c95d933b2f59ff9f841e2d4ccf1b
purin52002 <purin7635@gmail.com>
2020-01-24 21:43:10 +0900

    fix

--- config.py
+++ config.py
@@ -10,11 +10,14 @@ gdrive_path = Path(r'/content/drive/My Drive')
 
 
 class ImagePath:
+    jpg_image_name_list = ['salad', 'katsudon',
+                           'farm', 'flower', 'watarfall']
     image_dir_path = gdrive_path/'Image'
     image_path_dict = {image_name: str(image_dir_path/f'{image_name}.jpg')
-                       for image_name in ['salad', 'katsudon',
-                                          'farm', 'flower', 'watarfall']}
+                       for image_name in jpg_image_name_list}
 
 
 class DirectoryPath:
     weight = gdrive_path/'weight'
+    scored_param = gdrive_path/'scored_param'
+    tfrecords = Path(r'/content/tfrecords')
