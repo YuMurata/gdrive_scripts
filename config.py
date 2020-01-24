45bca14f7892a6c7356cb8afd8968d0f2a07e9ab
purin52002 <purin7635@gmail.com>
2020-01-24 19:32:57 +0900

    fix

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ class ImageInfo:
     shape = width, height, channel = 224, 224, 3
     size = width, height
 
+gdrive_path =  Path(r'/content/drive/My Drive')
 
 class ImagePath:
-    image_dir_path = Path(r'/content/drive/My Drive/Image')
+    image_dir_path = gdrive_path/'Image'
     image_path_dict = {'salad': str(image_dir_path/'salad.jpg'),
                        'katsudon': str(image_dir_path/'katsudon.jpg')}
 
 
 class DirectoryPath:
-    weight = Path('weight')
+    weight = gdrive_path/'weight'
